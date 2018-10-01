e6d0b7e699538880c7ef3dfca704ffad7925741c
Sebastião Miranda <sebastiao.miranda@outlook.com>
2018-10-01 22:50:25 +0100

    Update Mailer.cs

--- owlas_0_0_1/Classes/Mailer.cs
+++ owlas_0_0_1/Classes/Mailer.cs
@@ -14,10 +14,10 @@ namespace owlas_0_0_1.Classes
         {
             Mailgun.Init("key-4otkg7cfsi5qn9f$e9");
 
-            string confirmationGuid = user.ProviderUserKey.ToString();
-            string verifyUrl = HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Authority) + "/account/verify?ID=" + confirmationGuid;
-            string sender = "noreply@owlasdev.mailgun.org";
-            string recipients = user.Email;
+            var confirmationGuid = user.ProviderUserKey.ToString();
+            var verifyUrl = HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Authority) + "/account/verify?ID=" + confirmationGuid;
+            var sender = "noreply@owlasdev.mailgun.org";
+            var recipients = user.Email;
 
             MailgunMessage.SendText(sender, recipients, "Owlas - Confirmação de registo", "Para confirmares o teu endereço de email e ires para o owlas carrega neste link:\n\n" + verifyUrl + "\n\n\nA equipa owlas.com");
         }
